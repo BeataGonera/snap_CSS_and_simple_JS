5e3734845690c6dfd29c7b509c7870a37169adb0
Beata Gonera <beataiwonagonera@gmail.com>
2022-09-23 13:44:12 +0200

    features menu and company menu ready

---
+++ snap/main.js
@@ -0,0 +1,38 @@
+let featuresLink = document.getElementById('features-link')
+let featuresMenu = document.getElementById('features-menu')
+let featuresStroll = document.getElementById('features-stroll')
+
+let companyLink = document.getElementById('company-link');
+let companyMenu = document.getElementById('company-menu');
+let companyStroll = document.getElementById('company-stroll');
+let featuresClickCounter = 1;
+let companyClickCounter = 1;
+
+featuresLink.onclick = function showFeaturesMenu(){
+    
+    if(featuresClickCounter % 2 !==0){
+      featuresMenu.classList.add('features-menu-open');
+      featuresMenu.style.display = "flex";
+      featuresStroll.src = "/images/icon-arrow-up.svg";
+    }else{
+      featuresMenu.classList.remove('features-menu-open');
+      featuresMenu.style.display = "none";
+      featuresStroll.src = "/images/icon-arrow-down.svg";
+    }
+    return featuresClickCounter++
+}
+
+companyLink.onclick = function showCompanyMenu(){
+    
+    if(companyClickCounter % 2 !==0){
+      companyMenu.classList.add('company-menu-open');
+      companyMenu.style.display = "flex";
+      companyStroll.src = "/images/icon-arrow-up.svg";
+    }else{
+      companyMenu.classList.remove('company-menu-open');
+      companyMenu.style.display = "none";
+      companyStroll.src = "/images/icon-arrow-down.svg";
+    }
+    return companyClickCounter++
+}
+
