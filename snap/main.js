4e8263697cb42977a6b618e6fc6c36e369ff3401
Beata Gonera <beataiwonagonera@gmail.com>
2022-10-02 20:54:48 +0200

    bug fixed - pictures displaying properly in browser

--- snap/main.js
+++ snap/main.js
@@ -12,28 +12,28 @@ let companyClickCounter = 1;
 featuresLink.onmouseover = function showFeaturesMenu(){
     featuresMenu.classList.add('features-menu-open');
     featuresMenu.style.display = "flex";
-    featuresStroll.src = "/images/icon-arrow-up.svg";
+    featuresStroll.url = "./images/icon-arrow-up.svg";
 }
 
 featuresLink.onmouseleave = function hideFeaturesMenu(){
     if(featuresClickCounter % 2 !==0){
     featuresMenu.classList.remove('features-menu-open');
     featuresMenu.style.display = "none";
-    featuresStroll.src = "/images/icon-arrow-down.svg";
+    featuresStroll.src = "./images/icon-arrow-down.svg";
     }
 }
 
 companyLink.onmouseover = function showCompanyMenu(){
     companyMenu.classList.add('company-menu-open');
     companyMenu.style.display = "flex";
-    companyStroll.src = "/images/icon-arrow-up.svg";
+    companyStroll.src = "./images/icon-arrow-up.svg";
 }
 
 companyLink.onmouseleave = function hideCompanyMenu(){
     if(companyClickCounter % 2 !==0){
     companyMenu.classList.remove('company-menu-open');
     companyMenu.style.display = "none";
-    companyStroll.src = "/images/icon-arrow-down.svg";
+    companyStroll.src = "./images/icon-arrow-down.svg";
     }
 }
 
@@ -42,11 +42,11 @@ featuresLink.onclick = function displayFeaturesMenu(){
     if(featuresClickCounter % 2 !==0){
       featuresMenu.classList.add('features-menu-open');
       featuresMenu.style.display = "flex";
-      featuresStroll.src = "/images/icon-arrow-up.svg";
+      featuresStroll.src = "./images/icon-arrow-up.svg";
     }else{
       featuresMenu.classList.remove('features-menu-open');
       featuresMenu.style.display = "none";
-      featuresStroll.src = "/images/icon-arrow-down.svg";
+      featuresStroll.src = "./images/icon-arrow-down.svg";
     }
     return featuresClickCounter++
 }
@@ -56,11 +56,11 @@ companyLink.onclick = function displayCompanyMenu(){
     if(companyClickCounter % 2 !==0){
       companyMenu.classList.add('company-menu-open');
       companyMenu.style.display = "flex";
-      companyStroll.src = "/images/icon-arrow-up.svg";
+      companyStroll.src = "./images/icon-arrow-up.svg";
     }else{
       companyMenu.classList.remove('company-menu-open');
       companyMenu.style.display = "none";
-      companyStroll.src = "/images/icon-arrow-down.svg";
+      companyStroll.src = "./images/icon-arrow-down.svg";
     }
     return companyClickCounter++
 }
